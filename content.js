d38ff033a98351b26e7350d6a1e71d4ed3ff9767
Leg3ndary <ben.zhou.2006@gmail.com>
2024-10-04 23:07:45 -0400

    Add a funny logo

--- content.js
+++ content.js
@@ -1,6 +1,34 @@
-document.querySelectorAll("tr td:first-child").forEach((td) => {
+document.querySelectorAll("tr td:first-child").forEach((td, index) => {
   let lessonNumber = parseInt(td.textContent, 10);
+
   if (!isNaN(lessonNumber)) {
-    td.textContent = lessonNumber + 1;
+    td.textContent = "";
+
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.classList.add("custom-cb");
+
+    // for lessons
+    const checkboxKey = `lsn_${index}`;
+
+    const savedState = localStorage.getItem(checkboxKey);
+
+    if (savedState === "checked") {
+      checkbox.checked = true;
+    }
+
+    // on every change update
+    checkbox.addEventListener("change", function () {
+      if (checkbox.checked) {
+        localStorage.setItem(checkboxKey, "checked");
+      } else {
+        localStorage.setItem(checkboxKey, "unchecked");
+      }
+    });
+
+    const lessonText = document.createTextNode(lessonNumber + 1);
+
+    td.appendChild(checkbox);
+    td.appendChild(lessonText);
   }
 });
